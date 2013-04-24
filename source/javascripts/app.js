35c7147afc28138c993a3f76d59224e4608f0ae0
Patti <pj.97211@gmail.com>
2013-04-24 14:37:45 -0700

    final commitgit add -A

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -2,28 +2,44 @@
 
 var Apple = Backbone.Model.extend();
 
-var Apple = Backbone.Collection.extend({
-    model: Apple,
-    url: 'http://search.twitter.com/search.json?q=NYC&callback=?',
-    parse: function(response) {
-        console.log('parsing ...')
-        return response.results;
-    }
-});
-
 
 ////////////////////////////////////////////// Collection
 
 var Bushel = Backbone.Collection.extend({
-    model: Apple
+    model: Apple,
+    url: 'http://search.twitter.com/search.json?q=NYC&callback=?',
+    parse: function(response) {
+        // console.log('parsing ...')
+        return response.results;
+    }
 });
 
-
+bushel = new Bushel();
 
 ////////////////////////////////////////////// Views
 
 (function ($) {
 
+  var ListView = Backbone.View.extend({
+    el: $("body"),
+
+    initialize: function () {
+        console.log("working?");
+    },
+    events: {
+      "click #mybutton":  "addOne"
+    },
+    addOne: function () {
+      // var friend_name = prompt("Who is your friend?");
+      // this.bushel.fetch();
+      document.getElementById("content").innerHTML = bushel;
+    }
+  });
+
+
+$(document).ready(function(){
+  var pageView = new ListView();
+});
 
 
 })(jQuery);
